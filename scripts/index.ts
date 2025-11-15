7682361adf4f5e5c97f10cb091826179c484da6d
Linus Fischer <leicraft@leicraftmc.de>
2025-11-15 02:33:08 +0100

    update

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,10 +1,12 @@
 import { CLIApp } from "@cleverjs/cli"
 import { PublishCMD } from "./cmds/publish"
+import { BuildCMD } from "./cmds/build";
 
 class CLI extends CLIApp {
 
     protected override onInit() {
         this.register(new PublishCMD());
+        this.register(new BuildCMD());
     }
 
 }
