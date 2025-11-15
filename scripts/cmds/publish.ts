070a192ed8f688831d2ac23d168b43ce35982bd2
Linus Fischer <leicraft@leicraftmc.de>
2025-11-15 02:02:54 +0100

    update

--- scripts/cmds/publish.ts
+++ scripts/cmds/publish.ts
@@ -13,21 +13,50 @@ export class PublishCMD extends CLICMD {
         const GIT_DEPLOY_KEY_ID = Utils.requireEnvVariable("GIT_DEPLOY_KEY_ID");
         const GIT_DEPLOY_KEY_SECRET = Utils.requireEnvVariable("GIT_DEPLOY_KEY_SECRET");
 
-        const files = this.getFilesWithVersion(await fs_readdir("."));
+        const files = this.checkFiles(await fs_readdir("."));
         console.log("Found files:", files);
     }
 
-    private getFilesWithVersion(files: string[]): { file: string, version: string }[] {
+    private async checkFiles(files: string[]) {
+
+        const versions: Record<string, string[]> = {};
+
         const regex = /^leios-([0-9]+\.[0-9]+\.[0-9]+)-amd64\..*$/;
-        const result: { file: string, version: string }[] = [];
         for (const file of files) {
             const match = file.match(regex);
             if (match) {
                 const version = match[1];
-                result.push({ file, version: version as string });
+                if (typeof version !== "string") {
+                    continue;
+                }
+                
+                if (!versions[version]) {
+                    versions[version] = [];
+                }
+                versions[version].push(file);
             }
         }
-        return result;
+
+        const results: Record<string, string[]> = {};
+
+        for (const [version, versionFiles] of Object.entries(versions)) {
+
+            const requiredFiles = [
+                `leios-${version}-amd64.hybrid.iso`,
+                `leios-${version}-amd64.files`,
+                `leios-${version}-amd64.packages`,
+                `leios-${version}-amd64.contents`,
+            ];
+
+            if (!requiredFiles.every(f => versionFiles.includes(f))) {
+                continue;
+            }
+            if (versionFiles.length !== requiredFiles.length) {
+                continue;
+            }
+            results[version] = versionFiles;
+        }
+        return results;
     }
 
 }
