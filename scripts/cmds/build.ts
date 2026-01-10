e41849eed8d282d5e3839d8edb5e16752be98c5a
Linus Fischer <leicraft@leicraftmc.de>
2026-01-10 15:05:39 +0000

    update

--- scripts/cmds/build.ts
+++ scripts/cmds/build.ts
@@ -1,4 +1,4 @@
-import { CLICMD } from "@cleverjs/cli";
+import { CLICMD, CMDFlag, CMDFlagsParser } from "@cleverjs/cli";
 import { Utils } from "../utils";
 
 export class BuildCMD extends CLICMD {
@@ -7,9 +7,43 @@ export class BuildCMD extends CLICMD {
     override description = "Build the LeiOS live images.";
     override usage = "build";
 
-    override async run(args: string[], meta: any) {
-        // await Utils.execNativeCommand(["lb", "clean", "--purge"], { cwd: "live-build" });
-        // await Utils.execNativeCommand(["lb", "config"], { cwd: "live-build" });
-        await Utils.execNativeCommand(["lb", "build"], { cwd: "./tmp/build" });
+    private flagParser = new CMDFlagsParser({
+        "--version": new CMDFlag("string", "The version of the LeiOS live image to build.", true, null),
+        "--architecture": new CMDFlag("string", "The target architecture to publish (amd64 or arm64).", true, null)
+    })
+
+    override async run(args: string[]) {
+
+        const parsedFlags = this.flagParser.parse(args);
+        if (typeof parsedFlags === "string") {
+            console.error("Error parsing flags:", parsedFlags);
+            return;
+        }
+
+        const version = parsedFlags["--version"];
+        if (typeof version !== "string" || version.length === 0) {
+            console.error("Invalid version specified.");
+            console.error("Usage:", "--base-dir=<dir> --architecture=<amd64|arm64> --version=<version>");
+            return;
+        }
+
+        const architecture = parsedFlags["--architecture"];
+        if (architecture !== "amd64" && architecture !== "arm64") {
+            console.error("Invalid architecture specified. Must be 'amd64' or 'arm64'.");
+            console.error("Usage:", "--base-dir=<dir> --architecture=<amd64|arm64> --version=<version>");
+            return;
+        }
+
+        await Utils.createTMPBuildDir();
+
+        await Utils.execNativeCommand(["lb", "build"], {
+            cwd: "./tmp/build",
+            env: {
+                "INSERT_TARGET_ARCH": architecture,
+                "INSERT_TARGET_LIVE_VERSION": version,
+                //@TODO do a better solution for codename detection in the future
+                "INSERT_BASE_CODENAME": "trixie",
+            }
+        });
     }
 }
