0b522c93d0de52c6081c3346246750d04c712bba
Linus Fischer <leicraft@leicraftmc.de>
2026-01-10 14:44:03 +0000

    update

--- scripts/cmds/build.ts
+++ scripts/cmds/build.ts
@@ -1,4 +1,4 @@
-import { CLICMD } from "@cleverjs/cli";
+import { CLICMD, CMDFlag, CMDFlagsParser } from "@cleverjs/cli";
 import { Utils } from "../utils";
 
 export class BuildCMD extends CLICMD {
@@ -7,9 +7,27 @@ export class BuildCMD extends CLICMD {
     override description = "Build the LeiOS live images.";
     override usage = "build";
 
+    private flagParser = new CMDFlagsParser({
+        "--base-dir": new CMDFlag("string", "The base directory where the built files are located.", true, null),
+    })
+
     override async run(args: string[], meta: any) {
+
+        const parsedFlags = this.flagParser.parse(args);
+        if (typeof parsedFlags === "string") {
+            console.error("Error parsing flags:", parsedFlags);
+            return;
+        }
+
+        const baseDir = parsedFlags["--base-dir"];
+        if (typeof baseDir !== "string" || baseDir.length === 0) {
+            console.error("Invalid base-dir specified.");
+            console.error("Usage:", "--base-dir=<dir>");
+            return;
+        }
+
         // await Utils.execNativeCommand(["lb", "clean", "--purge"], { cwd: "live-build" });
         // await Utils.execNativeCommand(["lb", "config"], { cwd: "live-build" });
-        await Utils.execNativeCommand(["lb", "build"], { cwd: "live-build" });
+        await Utils.execNativeCommand(["lb", "build"], { cwd: baseDir });
     }
 }
--- scripts/cmds/clean.ts
+++ scripts/cmds/clean.ts
@@ -1,4 +1,4 @@
-import { CLICMD } from "@cleverjs/cli";
+import { CLICMD, CMDFlag, CMDFlagsParser } from "@cleverjs/cli";
 import { Utils } from "../utils";
 
 export class CleanCMD extends CLICMD {
@@ -7,7 +7,24 @@ export class CleanCMD extends CLICMD {
     override description = "Clean the LeiOS live images.";
     override usage = "clean";
 
+    private flagParser = new CMDFlagsParser({
+        "--base-dir": new CMDFlag("string", "The base directory where the built files are located.", true, null),
+    })
+
     override async run(args: string[], meta: any) {
-        await Utils.execNativeCommand(["lb", "clean", "--purge"], { cwd: "live-build" });
+
+        const parsedFlags = this.flagParser.parse(args);
+        if (typeof parsedFlags === "string") {
+            console.error("Error parsing flags:", parsedFlags);
+            return;
+        }
+
+        const baseDir = parsedFlags["--base-dir"];
+        if (typeof baseDir !== "string" || baseDir.length === 0) {
+            console.error("Invalid base-dir specified.");
+            console.error("Usage:", "--base-dir=<dir>");
+            return;
+        }
+        await Utils.execNativeCommand(["lb", "clean", "--purge"], { cwd: baseDir });
     }
 }
